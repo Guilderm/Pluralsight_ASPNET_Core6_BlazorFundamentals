8af8b08fb98ea1f05699e9f49ac8d89e1e60e110
Guilder W. Milliner <guilderw@gmail.com>
2023-01-30 23:20:05 -0600

    End of module 3

--- BethanysPieShopHRM.App/BethanysPieShopHRM.App/Pages/EmployeeOverview.razor.cs
+++ BethanysPieShopHRM.App/BethanysPieShopHRM.App/Pages/EmployeeOverview.razor.cs
@@ -5,6 +5,9 @@ namespace BethanysPieShopHRM.App.Pages
 {
     public partial class EmployeeOverview
     {
+        private const string SMALL_SIZE = "-small.jpg";
+        private const string IMAGE_URL = "https://gillcleerenpluralsight.blob.core.windows.net/person/";
+
         public List<Employee> Employees { get; set; } = default!;
 
 
